5dc164430d978ea353f979cd0b6c7a7539050123
namyesol <studhaza@naver.com>
2023-09-26 22:54:08 +0900

    [#4] spring-security 설정
    - AuthenticationProvider : CustomUserDetailsService 사용하도록 설정했음
    - UserService: UserDetailsService를 구현해서 인증을 진행할 사용자를 AuthenticationProvider가 찾도록함.
    - User: UserDetails를 상속했음.
    - PasswordEncoder : BCryptPasswordEncoder를 사용해서 사용자 가입 시 비밀번호를 암호화함
    - TransactionManager@proxy-target-class : 클래스의 바이트코드를 조작해 프록시 객체를 생성할 수 있도록 함

--- src/main/java/kr/or/connect/reservation/domain/user/User.java
+++ src/main/java/kr/or/connect/reservation/domain/user/User.java
@@ -1,10 +1,16 @@
 package kr.or.connect.reservation.domain.user;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
-public class User {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class User implements UserDetails {
     
     private Integer id;
     private String email;
@@ -97,5 +103,40 @@ public class User {
         if (!this.getRoles().contains(role)) {
             this.getRoles().add(role);
         }
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        ArrayList<Role> roles = new ArrayList<>(getRoles());
+        for (Role role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+       return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
